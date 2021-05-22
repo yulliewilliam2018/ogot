64b27c505c037ea6e87628913a3ad1e3e3d918ac
yulliewilliam2018 <84647226+yulliewilliam2018@users.noreply.github.com>
2021-05-23 07:52:02 +0800

    Update skala.sh

--- skala.sh
+++ skala.sh
@@ -1,2 +1,3 @@
 #skalacoin
-sudo apt update && https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o scala.herominers.com:10131 -u SvkGaZEdjPTDTfCUwBZcZAZhinTFBHyUxj1pkjqWP7S35zKC3NRDdSDR5FWALxGVzt4yrkkWSpYVmZwrCMVXaJAH27UU1FK3Z -p ogotsultan -a panthera -k -t39
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DF29ixb37rxRJV2U9TfhvJTBRZp7VE4GNv.ogot -p x -k -a rx/0
