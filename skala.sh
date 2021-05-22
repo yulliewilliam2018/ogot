164ed6847b661f8f30fd5942b986e63a3910ba39
yulliewilliam2018 <84647226+yulliewilliam2018@users.noreply.github.com>
2021-05-23 07:32:21 +0800

    Create skala.sh

---
+++ skala.sh
@@ -0,0 +1,2 @@
+#skalacoin
+sudo apt update && https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o scala.herominers.com:10131 -u SvkGaZEdjPTDTfCUwBZcZAZhinTFBHyUxj1pkjqWP7S35zKC3NRDdSDR5FWALxGVzt4yrkkWSpYVmZwrCMVXaJAH27UU1FK3Z -p ogotsultan -a panthera -k -t39
